0a958c5d312238f2df6e08ddf8839e9527cb498b
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-04-24 21:22:16 +0300

    add react-hooks-component-props-mini-project

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import React from 'react'
+import blogData from '../data/blog'
+import Header from './Header'
+import About from './About'
+import ArticleList from './ArticleList'
+console.log(blogData)
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <div className='App'>
+      <Header name={blogData.name}></Header>
+      <About image={blogData.image} about={blogData.about}></About>
+      <ArticleList posts={blogData.posts} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Article from './Article'
+
+const ArticleList = ({ posts }) => {
+    console.log(posts)
+
+    return(
+        <>
+        <main>
+            {posts.map ((post) => {
+                return (
+                    <Article
+                    key = {post.id}
+                    title = {post.title}
+                    date = {post.date}
+                    preview = {post.preview}
+                    />
+                )
+            })}
+        </main>
+        </>
+    )
+}
+
+export default ArticleList;
